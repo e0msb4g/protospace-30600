b657122cd7c2ae2cdb745a8daa352e734b8bd430
e0msb4g <koki.wind194@gmail.com>
2020-10-19 13:25:56 +0900

    Update 20201018055327_create_comments.rb

--- db/migrate/20201018055327_create_comments.rb
+++ db/migrate/20201018055327_create_comments.rb
@@ -5,6 +5,6 @@ class CreateComments < ActiveRecord::Migration[6.0]
       t.text :text, null: false
       t.references :user,       null: false, foreign_key: true
       t.references :prototype,  null: false, foreign_key: true
-
+    end
   end
 end
